b40417dc8aea056449438d76ce4a381ef2286932
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 17:27:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,58 @@ if (process.env.GITHUB_REF) {
 var fileOptions = {
     folder: process.env.LIB_FOLDER, 
     fileName: `HPC ATMP Response.pbix`,
-    fileContent: fs.readFileSync(process.env.FILE_PATH1),
+    fileContent: fs.readFileSync(process.env.FILE_PATH1)
+};
+
+spsave(coreOptions, creds, fileOptions)
+.then(function(){
+    console.log('Success');
+})
+.catch(function(err){
+    process.exit(1);
+}) 
+var fileOptions = {
+    folder: process.env.LIB_FOLDER, 
     fileName: `HPC Adv Package Response.pbix`,
-    fileContent: fs.readFileSync(process.env.FILE_PATH2),
-    fileName: `Substrate Response.pbix.`,
+    fileContent: fs.readFileSync(process.env.FILE_PATH2)
+};
+
+spsave(coreOptions, creds, fileOptions)
+.then(function(){
+    console.log('Success');
+})
+.catch(function(err){
+    process.exit(1);
+})
+var fileOptions = {
+    folder: process.env.LIB_FOLDER, 
+    fileName: `HPC Bump_Sort_BSM Response.pbix.`,
+    fileContent: fs.readFileSync(process.env.FILE_PATH3)
+};
+
+spsave(coreOptions, creds, fileOptions)
+.then(function(){
+    console.log('Success');
+})
+.catch(function(err){
+    process.exit(1);
+})
+var fileOptions = {
+    folder: process.env.LIB_FOLDER, 
+    fileName: `HPC Demand Requirements.pbix`,
+    fileContent: fs.readFileSync(process.env.FILE_PATH4)
+};
+
+spsave(coreOptions, creds, fileOptions)
+.then(function(){
+    console.log('Success');
+})
+.catch(function(err){
+    process.exit(1);
+})
+var fileOptions = {
+    folder: process.env.LIB_FOLDER, 
+    fileName: `Substrate Response.pbix`,
     fileContent: fs.readFileSync(process.env.FILE_PATH5)
 };
 
@@ -36,4 +84,18 @@ spsave(coreOptions, creds, fileOptions)
 })
 .catch(function(err){
     process.exit(1);
-});
+})    
+var fileOptions = {
+    folder: process.env.LIB_FOLDER, 
+    fileName: `Wafer Response.pbix`,
+    fileContent: fs.readFileSync(process.env.FILE_PATH6)
+};
+
+spsave(coreOptions, creds, fileOptions)
+.then(function(){
+    console.log('Success');
+})
+.catch(function(err){
+    process.exit(1);
+}); 
+      
