9d421a470074f264baa701a04c63eca3ca0334e3
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 16:59:11 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ echo "Creating archive";
 mkdir /out
 cd $GITHUB_WORKSPACE
 cp "HPC ATMP Response.pbix" /out
-cp "HPC Adv Package Response.pbix " /out
+cp "HPC Adv Package Response.pbix" /out
 cp "HPC Bump_Sort_BSM Response.pbix" /out
 cp "HPC Demand Requirements.pbix" /out
-cp "Substrate Response.pbix " /out
+cp "Substrate Response.pbix" /out
 cp "Wafer Response.pbix" /out
 
 export FILE_PATH='/out/*'
