9f2e438c20022baca354c527e7a08cff8597b57c
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 16:17:26 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Creating archive";
 mkdir /out
 cd $GITHUB_WORKSPACE
 cp "HPC ATMP Response.pbix" /out
-export FILE_PATH='/out/*'
+export FILE_PATH='/out/HPC ATMP Response.pbix'
 
 
 node /app/index.js
