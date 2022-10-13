3d86551a841f51de8bdbe404810157005ad67346
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 13:01:56 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Creating archive";
 mkdir /out
 cd $GITHUB_WORKSPACE
-unzip -r /out/repoarchive.zip ./* -x .git/*
-export FILE_PATH='/out/repoarchive'
+zip -r /out/repoarchive.zip ./* -x .git/*
+export FILE_PATH='/out/repoarchive.zip'
 
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
