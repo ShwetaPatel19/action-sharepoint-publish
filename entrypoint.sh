e8a4e0b7b51a166927df42514115ca76b2aef772
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 17:15:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,14 @@ cp "HPC Demand Requirements.pbix" /out
 cp "Substrate Response.pbix" /out
 cp "Wafer Response.pbix" /out
 
-export FILE_PATH='/out'
+export FILE_PATH1='/out/HPC ATMP Response.pbix'
+export FILE_PATH2='/out/HPC Adv Package Response.pbix'
+export FILE_PATH3='/out/HPC Bump_Sort_BSM Response.pbix'
+export FILE_PATH4='/out/HPC Demand Requirements.pbix'
+export FILE_PATH5='/out/Substrate Response.pbix'
+export FILE_PATH6='/out/Wafer Response.pbix'
+
+
 
 
 node /app/index.js
