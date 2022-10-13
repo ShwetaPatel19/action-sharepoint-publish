55d9b2e6b21dd30f872cf4eca693d38632c5cce6
ShwetaPatel19 <112570596+ShwetaPatel19@users.noreply.github.com>
2022-10-13 15:39:03 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Creating archive";
 mkdir /out
 cd $GITHUB_WORKSPACE
-cp ./* -x .git/*  /out/repoarchive
-export FILE_PATH= '/out/repoarchive'
+cp HPC ATMP Response.pbix /out/*
+export FILE_PATH= '/out/*'
 
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
